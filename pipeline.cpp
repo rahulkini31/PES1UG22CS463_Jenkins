d1d19336d41a9d54c5271e522d26b25d4e480cfe
Rahul Kini <rahulkini369@gmail.com>
2025-03-18 16:46:16 +0530

    Create pipeline.cpp

---
+++ pipeline.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World\n";
+  cout << "Hello, Pipeline\n";
+  cout << " I have created this print statment for the task five";
+  return 0;
+}
